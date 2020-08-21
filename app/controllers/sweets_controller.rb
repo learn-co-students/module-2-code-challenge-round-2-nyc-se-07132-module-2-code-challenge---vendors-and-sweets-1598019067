08ad7f34d64f8ff309f266a52a8298683638a803
dkim61 <dkim187@binghamton.edu>
2020-08-21 11:46:48 -0400

    dk added some small stuff

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,10 +1,17 @@
 class SweetsController < ApplicationController
+before_action :find_sweet, only: [:show]
+
   def index
     @sweets = Sweet.all
   end
 
   def show
-    @sweet = Sweet.find(params[:id])
   end
+
+private
+
+def find_sweet
+  @sweet = Sweet.find(params[:id])
+end
   
 end
