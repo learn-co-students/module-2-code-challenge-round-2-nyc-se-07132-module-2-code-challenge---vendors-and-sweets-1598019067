e0355ebef5a06e794fd6fc43702d6a80a81b9bc8
kevin_xie <xie.kevin.j@gmail.com>
2020-08-21 11:50:36 -0400

    uniqueness_advanced_done

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,14 +1,17 @@
 class VendorsController < ApplicationController
 
+  def new
+    @vendor = Vendor.new
+    #trying to make average work
+  end
+
   def index
     @vendors = Vendor.all
-    
-    
+    # total_average_sweet_price = @vendor.sweets.average(:price).round(2)
   end
 
   def show
     @vendor = Vendor.find(params[:id])
-    # @average_sweet_price = @vendor.sweets.average(:price).round(2)
   end
   
 end
--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -3,6 +3,7 @@ class VendorSweet < ApplicationRecord
     belongs_to :sweet
     validates :price, presence: true
     validates :price, numericality: {greater_than: 0}
-    # validates :uniqueness_of :vendor_sweet.id, scope: :vendor_id, :message => "Combo already exists"
+    validates_uniqueness_of :sweet_id, :scope => [:vendor_id], :message => "Combo already exists"
+
 end
 
